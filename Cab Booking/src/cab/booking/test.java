a48657ceeb8b62936cb8a00bcff58ba82baea638
sheyril <40118763+sheyril@users.noreply.github.com>
2019-11-06 21:04:13 +0530

    added all three tables

--- Cab Booking/src/cab/booking/test.java
+++ Cab Booking/src/cab/booking/test.java
@@ -1,9 +1,10 @@
 import java.sql.*;
 
 public class test {
-   public static void createTables(){
-      Connection c = null;
-      
+   private static Connection c = null;
+   private static Statement stmt = null;
+
+   public static void connectToDB(){
       try {
          Class.forName("org.sqlite.JDBC");
          c = DriverManager.getConnection("jdbc:sqlite:test.db");
@@ -12,23 +13,68 @@ public class test {
          System.exit(0);
       }
       System.out.println("Opened database successfully");
+   }
 
+   public static void createTableUsers(){
+      connectToDB();
       try{
          String userTable = "CREATE TABLE users (" + "username TEXT," 
                                                    + "userID INTEGER PRIMARY KEY,"
                                                    + "phone_num INTEGER," 
                                                    + "emailID BLOB," 
                                                    + "pswrd BLOB," 
+                                                   + "wallet INTEGER," 
                                                    + "trip_status TEXT);" ; 
-         String driversTable = "CREATE TABLE drivers (" + "name TEXT," 
-                                                   + "phone_num INTEGER,"
-                                                   + "rating INTEGER," 
-                                                   + "trip_status TEXT);" ; 
+        
          try{
-            PreparedStatement stmt = c.prepareStatement(userTable);
-             System.out.println(stmt.execute());
-             stmt = c.prepareStatement(driversTable);
-             System.out.println(stmt.execute());
+            stmt = c.createStatement();
+            stmt.executeUpdate(userTable);
+         } catch (SQLException e){
+            System.out.println("SQLException");
+         }
+         c.close();
+      }catch ( Exception e ) {
+         System.out.println("Error occured");
+         System.exit(1);
+      }
+      System.out.println("Table created successfully");
+   }
+
+   public static void createTableDrivers(){
+      connectToDB();
+      try{
+         String userDrivers = "CREATE TABLE drivers (" + "username TEXT," 
+                                                   + "rating INTEGER ,"
+                                                   + "phone_num INTEGER," 
+                                                   + "emailID BLOB,"  
+                                                   + "status TEXT);" ; 
+        
+         try{
+            stmt = c.createStatement();
+            stmt.executeUpdate(userDrivers);
+         } catch (SQLException e){
+            System.out.println("SQLException");
+         }
+         c.close();
+      }catch ( Exception e ) {
+         System.out.println("Error occured");
+         System.exit(1);
+      }
+      System.out.println("Table created successfully");
+   }
+ 
+   public static void createTableLocations(){
+      connectToDB();
+      try{
+         String locations = "CREATE TABLE locations (" + "src_loc TEXT," 
+                                                   + "dest_loc TEXT ,"
+                                                   + "fare INTEGER," 
+                                                   + "num_drivers INTEGER,"  
+                                                   + "time INTEGER);" ; 
+        
+         try{
+            stmt = c.createStatement();
+            stmt.executeUpdate(locations);
          } catch (SQLException e){
             System.out.println("SQLException");
          }
@@ -41,6 +87,8 @@ public class test {
    }
 
    public static void main( String args[] ) {
-      createTables();
+      createTableUsers();
+      createTableDrivers();
+      createTableLocations();
    }
 }
